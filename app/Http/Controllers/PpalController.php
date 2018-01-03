33bf817aa7745a616afc9045405887d3d928de56
JSebas95 <sebasnieto95@hotmail.com>
2018-01-03 17:00:07 -0500

    migraciones y plantillas ready

--- app/Http/Controllers/PpalController.php
+++ app/Http/Controllers/PpalController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class PpalController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+      if($request){
+        $query=$request->get('searchText');
+        if(is_null($query)){
+          $cliente=Cliente::all();
+        }else{
+          $user_id=Cliente::where('cc',$query)->get();
+        }
+      }
       return view('ppal.pago.index');
     }
 }
--- resources/views/ppal/pago/index.blade.php
+++ resources/views/ppal/pago/index.blade.php
@@ -1,4 +1,5 @@
 @extends ('layouts.admin')
 @section('contenido')
+@include('cartera.deuda.search')
 <h3>Index</h3>
 @stop
