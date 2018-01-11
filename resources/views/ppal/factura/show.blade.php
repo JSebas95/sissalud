458688ef96ed87c54dc749a8d382c0255855356a
JSebas95 <sebasnieto95@hotmail.com>
2018-01-11 17:52:02 -0500

    correcion de errores menores

--- resources/views/ppal/factura/show.blade.php
+++ resources/views/ppal/factura/show.blade.php
@@ -18,9 +18,9 @@
                 <tr style="background-color:lightgrey; font-weight:bold">
                     <td>Fecha pago</td>
                     <td>Documento</td>
-                    <td>Usuario</td>
-                    <td>Fecha deuda</td>
-                    <td>Valor pagado</td>
+                    <td>Nombre</td>
+                    <td>Apellido</td>
+                    <td>Telefono</td>
                     <td>Estado deuda</td>
                     <!--td colspan="2"></td-->
                 </tr>
@@ -29,17 +29,19 @@
             @foreach($pago as $pag)
                 <tr>
                     <td>{{ $pag->creacion}}</td>
+                    <td>{{ $pag->cliente->cc }}</td>
                     <td>{{ $pag->cliente->nombre }}</td>
                     <td>{{ $pag->cliente->apellido }}</td>
-                    <td>{{ $pag->cliente->cc }}</td>
-                    <td>{{ $pag->telefono }}</td>
+
+
+                    <td>{{ $pag->cliente->telefono }}</td>
                     <td>${{ $pag->valor }}</td>
 
                 </tr>
             @endforeach
               <tr style="background-color:lightgrey; font-weight:bold">
                 <td><h4>Total</h4></td>
-                <td colspan='3'></td>
+                <td colspan='4'></td>
                 <td><h4>${{ number_format($total_pagado) }}</h4></td>
                 <td colspan='1'></td>
               </tr>
